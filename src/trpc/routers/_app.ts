b59d35d865c16bbbd6de19d610b94b9f4770f26d
将军 <2637953403@qq.com>
2026-07-08 14:45:09 +0800

    16:meetings setup

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,10 @@
 import { createTRPCRouter } from '../init';
 import { agentsRouter } from '@/modules/agents/server/procedures';
-import { agents } from '@/db/schema';
+import { meetingsRouter } from '@/modules/meetings/server/procedures';
 
 export const appRouter = createTRPCRouter({
-    agents: agentsRouter
+    agents: agentsRouter,
+    meetings: meetingsRouter
 });
 
 // export type definition of API
