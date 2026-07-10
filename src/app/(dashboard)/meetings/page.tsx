0f356bd7f3e7922082b02b228aa56993d5b1268a
将军 <2637953403@qq.com>
2026-07-10 10:12:26 +0800

    17:meetings form

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -8,18 +8,31 @@ import {
     MeetingsViewError,
     MeetingsViewLoading
 } from "@/modules/meetings/ui/views/meetings-view";
+import { MeetingsListHeader } from "@/modules/meetings/ui/components/meetings-list-header";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 
-const Page = () => {
+const Page = async () => {
+
+
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (!session) {
+        redirect("/sign-in")
+    }
 
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(
         trpc.meetings.getMany.queryOptions({})
     )
 
-
     return (
-        <div>
+        <>
+            <MeetingsListHeader />
             <HydrationBoundary state={dehydrate(queryClient)}>
                 <Suspense fallback={<MeetingsViewLoading />}>
                     <ErrorBoundary fallback={<MeetingsViewError />}>
@@ -27,7 +40,7 @@ const Page = () => {
                     </ErrorBoundary >
                 </Suspense >
             </HydrationBoundary >
-        </div >
+        </>
     )
 };
 
--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -12,7 +12,7 @@ export const MeetingsView = () => {
 
     return (
         <div>
-            {JSON.stringify(data?.items)}
+            TODO
         </div>
     )
 }
